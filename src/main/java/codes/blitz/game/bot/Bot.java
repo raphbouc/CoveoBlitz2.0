1f4a2326f4949eda5105ab70b604e64701a5f8a5
LamonGab <lamg0502@usherbrooke.ca>
2024-01-13 17:20:11 -0500

    Auto stash before merge of "Gab" and "origin/AlexOnTop"

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -41,11 +41,98 @@ public class Bot {
         cible = findFarthestEnemyShipPosition(myShip.worldPosition(), gameMessage.shipsPositions());
 
         List<Crewmate> idleCrewmates = new ArrayList<>(myShip.crew());
+        List<Crewmate> idleCrewmates_temp = new ArrayList<>(myShip.crew());
+
         //idleCrewmates.removeIf(crewmate -> crewmate.currentStation() != null || crewmate.destination() != null);
 
-        // Assignez 2 Crewmates aux stations de bouclier si disponibles
+
+        List<StationDistance> visitableShields = new ArrayList<>();
         List<ShieldStation> shieldStations = new ArrayList<>(myShip.stations().shields());
-        for (int i = 0; i < Math.min(2, idleCrewmates.size()) && !shieldStations.isEmpty(); i++) {
+        List<ShieldStation> shieldStations_temp = shieldStations;
+        List<TurretStation> turretStations = new ArrayList<>(myShip.stations().turrets());
+        List<TurretStation> turretStations_temp = turretStations;
+        List<StationDistance> visitableTurrets = new ArrayList<>();
+        List<StationDistance> visitableHelms = new ArrayList<>();
+        List<StationDistance> visitableRadars = new ArrayList<>();
+        for (Crewmate crewmate : idleCrewmates) {
+            visitableShields.addAll(crewmate.distanceFromStations().shields());
+            visitableTurrets.addAll(crewmate.distanceFromStations().turrets());
+            visitableHelms.addAll(crewmate.distanceFromStations().helms());
+            visitableRadars.addAll(crewmate.distanceFromStations().radars());
+        }
+
+        int shieldI = 0;
+        for (Crewmate crewmate : idleCrewmates) {
+            if (shieldI < 2 && crewmate.distanceFromStations().shields().size() > 0 && crewmate.currentStation() == null) {
+                for (int i = 0; i < shieldStations_temp.size(); i++) {
+                    if (shieldStations_temp.get(i).operator() == null) {
+                        actions.add(new MoveCrewAction(crewmate.id(), shieldStations_temp.get(i).gridPosition()));
+                        shieldStations_temp.remove(i);
+                        idleCrewmates_temp.remove(crewmate);
+                        break;
+                    }
+                }
+                shieldI++;
+            }
+        }
+
+        for (Crewmate crewmate : idleCrewmates_temp) {
+            if (crewmate.distanceFromStations().turrets().size() > 0 && crewmate.currentStation() == null) {
+                for (int i = 0; i < turretStations_temp.size(); i++) {
+                    if (turretStations_temp.get(i).operator() == null) {
+                        actions.add(new MoveCrewAction(crewmate.id(), turretStations_temp.get(i).gridPosition()));
+                        turretStations_temp.remove(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        /*for (Crewmate crewmate : idleCrewmates) {
+            List<StationDistance> visitableStations = new ArrayList<>(crewmate.distanceFromStations().shields());
+            int i=0;
+
+            for (StationDistance stationDistance : visitableStations) {
+                int stationToMoveTo = stationDistance.distance();
+
+                if (distance1 > stationToMoveTo) {
+                    closer2 = closer1;
+                    closer1 = crewmate.id();
+                    distance2 = distance1;
+                    distance1 = stationToMoveTo;
+                    shieldStation1 = shieldStations.get(i);
+                    i++;
+                } else if (distance2 > stationToMoveTo) {
+                    if (shieldStations.get(i) != shieldStation1) {
+                        closer2 = crewmate.id();
+                        distance2 = stationToMoveTo;
+                        shieldStation2 = shieldStations.get(i);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        for (int i=0; i<2;i++) {
+            for (Crewmate crewmate : idleCrewmates) {
+                if (crewmate.id().equals(closer1) && shieldStation1 != null) {
+                    actions.add(new MoveCrewAction(closer1, shieldStation1.gridPosition()));
+                    idleCrewmates.remove(crewmate);
+                    break;
+                }
+
+                if (shieldStation2 != null && crewmate.id().equals(closer2)) {
+                    actions.add(new MoveCrewAction(closer2, shieldStation2.gridPosition()));
+                    idleCrewmates.remove(crewmate);
+                    break;
+                }
+            }
+        }
+
+
+
+        // Assignez 2 Crewmates aux stations de bouclier si disponibles
+        /*for (int i = 0; i < Math.min(2, idleCrewmates.size()) && !shieldStations.isEmpty(); i++) {
             Crewmate crewmate = idleCrewmates.get(i);
             ShieldStation shieldStation = shieldStations.get(i);
             actions.add(new MoveCrewAction(crewmate.id(), shieldStation.gridPosition()));
@@ -69,7 +156,7 @@ public class Bot {
                     break; // Sortir de la boucle interne une fois qu'un Crewmate est assigné
                 }
             }
-        }
+        }*/
 
         // Assigner les membres d'équipage aux stations d'armes
         for (Crewmate crewmate : idleCrewmates) {
@@ -98,6 +185,13 @@ public class Bot {
                 actions.add(new MoveCrewAction(flexibleCrewmateId, turretStation.gridPosition()));
                 isFlexibleCrewmateAtTurret = true; // Le membre est maintenant à une tourelle
             }
+        }*/
+            // Now crew members at stations should do something!
+            List<TurretStation> operatedTurretStations = new ArrayList<>(myShip.stations().turrets());
+            operatedTurretStations.removeIf(turretStation -> turretStation.operator() == null);
+            for (TurretStation turretStation : operatedTurretStations) {
+                actions.add(new TurretShootAction(turretStation.id()));
+            }
         } else if (myShip.currentShield() <= 25 && flexibleCrewmateId != null) {
             ShieldStation shieldStation = findAvailableShieldStation(myShip.stations().shields());
             if (shieldStation != null) {
@@ -120,23 +214,20 @@ public class Bot {
             actions.add(new TurretLookAtAction(turretStation.id(), cible.toVector()));
         }
 
-        // Ajoutez ici d'autres actions répétitives pour chaque tick
-        // Par exemple, réorienter le vaisseau, scanner avec le radar, etc.
-        // ...
-
-        return actions;
-    }
-
-    // Méthode pour trouver une station de tourelle disponible
-    private TurretStation findAvailableTurretStation(List<TurretStation> turretStations) {
-        for (TurretStation turretStation : turretStations) {
-            if (turretStation.operator() == null) {
-                return turretStation;
+            List<HelmStation> operatedHelmStation = new ArrayList<>(myShip.stations().helms());
+            operatedHelmStation.removeIf(helmStation -> helmStation.operator() == null);
+            for (HelmStation helmStation : operatedHelmStation) {
+                actions.add(new RotateShipAction(90));
             }
-        }
-        return null;
-    }
 
+            List<RadarStation> operatedRadarStations = new ArrayList<>(myShip.stations().radars());
+            operatedRadarStations.removeIf(radarStation -> radarStation.operator() == null);
+            for (RadarStation radarStation : operatedRadarStations) {
+                actions.add(new RadarScanAction(radarStation.id(), otherShipsIds.get(new Random().nextInt(otherShipsIds.size()))));
+            }
+
+            // You can clearly do better than the random actions above. Have fun!!
+            return actions;
     // Méthode pour trouver une station de bouclier disponible
     private ShieldStation findAvailableShieldStation(List<ShieldStation> shieldStations) {
         for (ShieldStation shieldStation : shieldStations) {
@@ -145,7 +236,10 @@ public class Bot {
             }
         }
         return null;
+
+        
     }
+
     // Méthode utilitaire pour trouver une TurretStation par type
     private TurretStation findTurretByType(List<TurretStation> turretStations, String turretType) {
         for (TurretStation turretStation : turretStations) {
